230935e7a39c6965bb6351e182076570e5247d29
matheusguerreiro <prosadeteclas@gmail.com>
2023-12-21 23:01:52 -0300

    adjustments in apiJsonServer.js

--- src/api/apiJsonServer.js
+++ src/api/apiJsonServer.js
@@ -3,19 +3,11 @@ const server = jsonServer.create();
 const router = jsonServer.router('../db/db.json');
 const middlewares = jsonServer.defaults();
 
-const sqlite3 = require('better-sqlite3');
-const db = sqlite3('../bibles/ARC.sqlite');
-
 server.use(middlewares);
-
-server.get('/books', (req, res) => {
-  const data = db.prepare('SELECT * FROM book').all();
-  res.json(data);
-});
 
 server.use(router);
 
-const port = 3001;
+const port = 3002;
 server.listen(port, () => {
   console.log(`API Server está rodando em http://localhost:${port}`);
 });
